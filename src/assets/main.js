8a3fcc4595ec25f2e943bfeed0b0db17832938a8
kiruxaspb <dvoreckovkirill@gmail.com>
2023-04-11 17:36:28 +0300

    iteract with faucet contract

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,5 @@
 // Backend application repository: https://github.com/singnet/agi-faucet-lambda
-const { backendUrl } = require("./../../config.json")
+const abi = JSON.parse('[{"inputs":[{"internalType":"address","name":"_agixTokenAddress","type":"address"},{"internalType":"address","name":"_rejuveTokenAddress","type":"address"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Distribution","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawal","type":"event"},{"inputs":[],"name":"agixToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"agixWithdrawalAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"agixBalance","type":"uint256"},{"internalType":"uint256","name":"rejuveBalance","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lockTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rejuveToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rejuveWithdrawalAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint8","name":"_tokenId","type":"uint8"}],"name":"requestTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"setLockTime","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"setWithdrawalAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]');
 
 window.connect = async() => {
   const button = document.getElementById("connect");
@@ -18,34 +18,31 @@ window.onload = async function () {
   }
 }
 
-window.githubLogin = () => {
-  const xhr = new XMLHttpRequest()
-  xhr.open("GET", backendUrl + "/appId")
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        window.location.href = "https://github.com/login/oauth/authorize?client_id=" + JSON.parse(xhr.response).clientId
-      } else {
-        const error = document.getElementById("error")
-
-        error.style.display = "block"
-        error.innerText = "Something went wrong! Reason: " + xhr.responseText
-      }
-    }
-  }
-  xhr.send()
-}
-
-window.sendAGI = () => {
+window.requsetTokens = async() => {
+  notification = document.getElementById("notification");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  console.log('provider');
+  const signer = await provider.getSigner();
+  console.log('signer');
+  const contract = new ethers.Contract('0x19570fbC4e05940960b0A44C5f771008Af7935A2', abi, signer);
+  console.log('contract');
   const radio = document.getElementsByName('radio');
-  let network;
+  let token;
   for (let i = 0; i < radio.length; i++) {
     if (radio[i].checked) {
-      network = radio[i].value;
+      token = radio[i].value;
     }
   }
-
-  sendRequest(network)
+  console.log(token);
+  if (token === 'agix') {
+    tx = await contract.requestTokens(0);
+    notification.style.display = "block"
+    notification.innerHTML = `<p>Success! <br />Hash: <a href="https://goerli.etherscan.io/tx/${tx.hash}" target="_blank">${tx.hash}</a></p>`
+  } else {
+    await contract.requestTokens(1);
+    notification.style.display = "block"
+    notification.innerHTML = `<p>Success!</p>`
+  }
 }
 
 function sendRequest(network) {
